2d96dc58802404f03a68734abe53a3110a7bed39
Gabriel <gabriel.wambugu@student.moringaschool.com>
2023-02-05 12:44:05 +0300

    Lab attempted

--- src/components/About.js
+++ src/components/About.js
@@ -1,14 +1,58 @@
 import React from "react";
+import user from "../data/user.js";
 
-function About() {
+let userdata = [];
+userdata.push(user);
+
+function UserResponse(props){
+  return <div>
+   <p>{props.bio}</p>
+  </div>
+  
+}
+
+function  RejectedResponse(prop){
+  return <div>
+    <p style={{display: 'none'}}>""</p>
+  </div>
+  
+}
+
+function Link(details){
+  return(
+  <div id="linkinfo">
+      <h3>Links</h3>
+      <a href="gitprofile" >{details.github}</a>
+      <a href="linkedprofile" >{details.linkedin}</a>
+  </div>
+  
+
+  )
+  
+  
+}
+
+
+function About(){
+
   return (
     <div id="about">
       <h2>About Me</h2>
-      <p>Put the bio in here</p>
+      <div>
+      {typeof userdata.forEach(info => info.bio) === "string"}{
+        <UserResponse bio={user.bio}/>
+      } 
+      <RejectedResponse bio={user.bio}/>
+      
+      </div>
+  
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
-      {/* add your <Links /> component here */}
+      <Link github={user.links.github} linkedin={user.links.linkedin}/>
     </div>
   );
 }
+ 
+
 
 export default About;
+
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,21 @@
 import React from "react";
+import user from "../data/user.js";
 
-function Home(props) {
+
+function User(prop){
+  return (
+  <div>
+    <h1 style={{ color: prop.color }}>
+        {prop.name} is a Web Developer from {prop.city}
+  </h1>
+  </div>
+)}
+
+
+function Home() {
   return (
     <div id="home">
-      <h1 style={{ color: props.color }}>
-        {props.name} is a Web Developer from {props.city}
-      </h1>
+     <User name={user.name} color={user.color} city={user.city} />
     </div>
   );
 }
